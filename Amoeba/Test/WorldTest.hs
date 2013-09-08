4a8649cee00312aa6def75a33f74633a359adade
Alexandr Granin <graninas@gmail.com>
2013-09-08 19:49:57 +0700

    - Tests improved.

--- Amoeba/Test/WorldTest.hs
+++ Amoeba/Test/WorldTest.hs
@@ -18,7 +18,8 @@ import World.Geometry
 import qualified World.Player as P
 import qualified World.Items.Stone as S
 import qualified World.Items.Border as B
-import qualified World.Items.Canyon as C 
+import qualified World.Items.Canyon as C
+import qualified World.Items.Karyon as K
 
 prop_emptyWorldMap iId seed p = let
     g = mkStdGen seed
@@ -30,13 +31,30 @@ items1 ps = let
     objects = concatMap (\(p, iId) -> S.stone iId P.stonePlayer p) (zip ps [1..])
     in packObjects objects
 
+-- These tests are bad, but for education - OK.
+
 prop_stepWithInactives seed ps = let
     g = mkStdGen seed
-    its = items1 ps
-    w = worldFromList its (length ps) g
+    w = worldFromList (items1 ps) (length ps) g
     (steppedW, anns) = stepWorld w
+    its = worldItems w
     newIts = worldItems steppedW
-    in L.sort newIts == L.sort (map snd its)
+    in L.sort newIts == L.sort its
+
+setupKaryonGrowingTest seed p energy = let
+    k = K.karyon 1 P.player1 energy p
+    g = mkStdGen seed
+    w = worldFromList (packObjects k) 1 g
+    (steppedW, anns) = stepWorld w(map snd 
+    in (worldItems w, worldItems steppedW)
+
+prop_karyonIsGrowing seed p = length newIts > length its
+  where
+    (its, newIts) = setupKaryonGrowingTest seed p 100
+    
+prop_karyonNotGrowing seed p = L.sort newIts == L.sort its
+  where
+    (its, newIts) = setupKaryonGrowingTest seed p 0  
 
 runTests :: IO Bool
 runTests = $quickCheckAll
