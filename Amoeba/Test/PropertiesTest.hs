6e1de08a9d9aa29d281e334901e4888d0ccd0628
Alexandr Granin <graninas@gmail.com>
2013-09-11 01:27:55 +0700

    - Working on properties design.

--- Amoeba/Test/PropertiesTest.hs
+++ Amoeba/Test/PropertiesTest.hs
@@ -13,3 +13,9 @@ prop_moving p dir = let property = moving dir . dislocation p
 
 prop_notMoving p dir = let property = dislocation p
                        in query dislocation (tick property) == p
+
+
+prop_energyProducing cap cur prod = let
+        property = producing prod . battery cap cur
+        expectedEnergy = min (prod + cur) cap
+    in query (battery.cur) (tick property) == expectedEnergy
--- Amoeba/World/Properties.hs
+++ Amoeba/World/Properties.hs
@@ -10,21 +10,36 @@ type Properties = [Property]
 type Target = Point
 type Speed = Int
 type Energy = Int
+type Capacity = Energy
 type Durability = Int
 
+data Passability = AbleToFly | AbleToCreep | AbleToDrill
+
+passability :: Passability -> Property
 dislocation :: Point -> Property
+battery :: Capacity -> Energy -> Property
+ownership :: Player -> Property
+durability :: Durability -> Property
+
+
+
 moving :: Direction -> Property
+
+-- Can we add not Energy but generic resource, and 'energy' - one of resources?
+
+consuming :: Energy -> Property
+producing :: Energy -> Property
+
+-- Property == Target?
+transmission :: Property -> Property
+
+permissibility :: Properties -> Property
+interaction :: Property -> Property -> Property
 
 -- (Properties -> Bool) == Condition
 pursuit :: (Properties -> Bool) -> Target -> Speed -> Property
 
-energetics :: Energy -> Property
-
-ownership :: Player -> Property
-interaction :: Property -> Property -> Property
-
 type ProductionAlg = Properties -> Property
 type PlacementAlg = Property -> Properties -> Point -> Bool 
 
-durability :: Durability -> Property
 fabric :: ProductionAlg -> PlacementAlg -> Property
