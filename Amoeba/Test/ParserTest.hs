25b670990565411ff7caf483431d7f5a2b87d605
Alexandr Granin <graninas@gmail.com>
2014-01-27 23:32:06 +0700

    - Minor fixes in test.

--- Amoeba/Test/ParserTest.hs
+++ Amoeba/Test/ParserTest.hs
@@ -9,6 +9,15 @@ import Test.QuickCheck.All
 import GameLogic.Data.Facade
 import GameLogic.Language.Parser
 
+parseExampe (testName, dataFile, res) p = do
+    f <- readFile dataFile
+    putStrLn $ "'" ++ f ++ "' -> "
+    case p f of
+        Left errMsg -> putStrLn errMsg
+        x -> print (if x == res then "Test passed: " ++ testName
+                    else "Test failed: " ++ testName)
+    putStrLn ""
+
 tests :: IO Bool
 tests = $quickCheckAll
 
@@ -16,20 +25,16 @@ runTests = tests >>= \passed -> putStrLn $
   if passed then "All tests passed."
             else "Some tests failed."
 
-parseExampe dataFile p = do
-    f <- readFile dataFile
-    putStrLn $ "'" ++ f ++ "' -> "
-    case p f of
-        Left errMsg -> putStrLn errMsg
-        x -> print x
-    putStrLn ""
+example1 = ("Example1", "./Raws/Items.its",
+            Right [Comment " General items",EmptyToken,Item "Karyon" [IntResource "lifebound" (0,5000),IntResource "durability" (100,100),IntResource "energy" (300,2000)],EmptyToken,Comment " Conductor",Item "Conductor" [IntResource "lifebound" (0,1000),IntResource "durability" (100,100),IntResource "energy" (0,100)]]
+            )
+
 
 main :: IO ()
 main = do
-    --runTests
-    
-    parseExampe "./Raws/Items.its" parse
-    parseExampe "./Raws/Item.its" parseItem
-    
+    runTests
+
+    parseExampe example1 parse
+
     putStrLn ""
 
