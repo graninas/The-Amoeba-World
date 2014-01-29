430a3e45b3a2ae8d1ceb9359928aaa1b46768470
Alexandr Granin <graninas@gmail.com>
2014-01-30 01:16:41 +0700

    - Build scripts improvements.

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+echo "Cleaning..."
+
+rm -fr "./.bin/*"
+
 echo "Building..."
 cd Amoeba
 ghc -threaded --make -outputdir ../.bin -o ../.bin/Amoeba Amoeba.hs
@@ -11,4 +15,4 @@ set +e
 
 echo "Copying..."
 rm -f "./Game/Amoeba"
-cp "./.bin/Amoeba" ./Game/
+cp "./.bin/Amoeba" "./Game/"
