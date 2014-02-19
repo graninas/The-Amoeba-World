7c552a01ca2948e2486dfa00e1b77e4b4f9b7a31
Alexandr Granin <graninas@gmail.com>
2014-02-20 01:38:06 +0700

    - Weird results with Type Families.

--- Amoeba/Test/TypeFamilyTest3.hs
+++ Amoeba/Test/TypeFamilyTest3.hs
@@ -22,26 +22,47 @@ instance Show (PropertyToken Int) where
 instance Show (PropertyToken (Int, Int)) where
     show (IntResource _ i) = show i
 
-class Prop a where
-    type Out a :: *
-    getProperty :: a -> Out a
+class Show a => Prop a where
+    type Out a b :: *
+    getProperty :: a -> Out a ()
+    printProperty :: a -> String
 
 instance Prop (PropertyToken a) where
-    type Out (PropertyToken a) = a
+    type Out (PropertyToken a) a = a
     getProperty (IntProperty _ k) = k
     getProperty (IntResource _ k) = k
+    printProperty (IntProperty _ k) = show k
+    printProperty (IntResource _ k) = show k
+
+instance Show (PropertyToken a) where
+    show (IntProperty _ k) = show k
+    show (IntResource _ k) = show k
 
 
+data Wrap = forall p. Prop p => MkWrap p
 
-token1 = IntProperty "int" 10
-token2 = IntResource "intResource" (10, 1000)
---tokens = [token1, token2]
+instance Show Wrap where
+    show (MkWrap p) = show p
+
+instance Prop Wrap where
+    --type Out Wrap a = PropertyToken a
+    getProperty (MkWrap p) = undefined
+    printProperty = undefined
+
+type PropList = [Wrap]
+
+token1 = MkWrap $ IntProperty "int" 10
+token2 = MkWrap $ IntResource "intResource" (10, 1000)
+tokens = [token1, token2] :: PropList
 
 main = do
-    let res1 = getProperty (IntProperty "a" 10)
-    let res2 = getProperty (IntResource "b" (20, 20))
-
-    print res1
-    print res2
+    print $ getProperty (IntProperty "a" 10)
+    print $ getProperty (IntResource "b" (20, 20))
+    print $ map show tokens
+    print tokens
+    
+    
+    let res1 = getProperty $ head tokens
+   -- putStrLn $ printProperty res1
 
     putStrLn "Ok."
