91169923ae1ff69fbae0d8f15c9072178a4b8338
Alexandr Granin <graninas@gmail.com>
2014-03-18 21:09:54 +0700

    - Translation test improved.

--- Amoeba/Test/TranslationTest.hs
+++ Amoeba/Test/TranslationTest.hs
@@ -8,17 +8,22 @@ import GameLogic.Data.World
 import Control.Monad (liftM)
 import Data.Either.Combinators
 import qualified Data.Map as M
+import qualified Data.Monoid as Monoid
 
 extractItemMap = liftM (trtObjectTemplateMap . snd)
 extractLog = liftM (trtLog . snd)
 extractResult = liftM fst
 
+tryRight :: Monoid.Monoid m => Either n m -> m
+tryRight (Right r) = r
+tryRight _ = Monoid.mempty
+
 main = do
     
     tokens <- readFile "./Data/Raws/World3.arf"
     let res = toWorld tokens
-    print $ extractLog res
-    print $ extractItemMap res
+    putStrLn $ unlines . tryRight $ extractLog res
+    print $ tryRight $ extractItemMap res
     print $ extractResult res
 
 
