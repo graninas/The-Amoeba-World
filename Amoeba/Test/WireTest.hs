cf5065457a9abbb32436e9d88fe1d917def17f93
Alexandr Granin <graninas@gmail.com>
2014-04-04 00:16:12 +0700

    - Some weird tests of wires.

--- Amoeba/Test/WireTest.hs
+++ Amoeba/Test/WireTest.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Main where
 
 import Control.Wire
@@ -9,25 +10,14 @@ import Prelude hiding ((.), id)
 --import qualified Graphics.UI.SDL as SDL
 --import qualified Graphics.UI.SDL.Primitives as SDL
 
-wire1 :: (HasTime t s) => Wire s () m a t
-wire1 = time
-
+--newtype GameStateTIO a = GameStateTIO (StateT () IO a)
+--  deriving (Functor, Monad, MonadIO)
+-- wrapGameState = GameStateTIO
 
 type GameStateTIO = StateT () IO
 type MyWire a b = Wire (Timed NominalDiffTime ()) () GameStateTIO a b
 
-newtype MyIoWire a = MyIoWire (MyWire () a)
-
-instance Monad MyIoWire where
-    return = MyIoWire . pure
-    ma >>= f = do
-        r <- ma
-        f r
-
-instance MonadIO MyIoWire where
-    liftIO = undefined
-
-startLoop :: MyWire () Int -> GameStateTIO Int
+--startLoop :: MyWire () Int -> GameStateTIO Int
 startLoop wire = do
     (delta, session') <- stepSession clockSession_
     (eitherResult, w') <- stepWire wire delta (Right ())
@@ -39,18 +29,32 @@ loop' (Right x) w = do
     liftIO $ putStrLn "In loop!"
     return x
 
+wire1 :: (HasTime t s) => Wire s () m a t
+wire1 = time
+
 wire2 :: MyWire () Int
 wire2 = for 4 . pure (1 :: Int)
 
-wire3 :: MyIoWire ()
-wire3 = do
-    liftIO $ putStrLn "In wire3!"
+-- mkGen_ :: Monad m => (a -> m (Either e b)) -> Wire s e m a b
+-- evalStateT :: StateT s IO a -> s -> IO s
+-- type GameStateTIO = StateT () IO
+
+--wire3 :: MyWire () Int
+wire3 = mkGen_ $ \x -> evalStateT effect ()
+  where
+     effect :: GameStateTIO (Either () Int)
+     effect = do
+        liftIO $ putStrLn "In wire3!"
+        return $ Right (19 :: Int)
 
 main :: IO ()
 main = do
     
-    res <- runStateT (startLoop wire2) ()
+    res <- runStateT (startLoop wire1) ()
     print res
+
+    res2 <- runStateT (startLoop wire3) ()
+    print res2
     
     testWire clockSession_ (for 1 . wire1)
     putStrLn "All Ok."
