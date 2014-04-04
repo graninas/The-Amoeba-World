3b783780d48c3e1f9444196ed2f17d7bd0301c9e
Alexandr Granin <graninas@gmail.com>
2014-04-04 23:11:40 +0700

    - New experiments.

--- Amoeba/Test/WireTest.hs
+++ Amoeba/Test/WireTest.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Main where
 
 import Control.Wire
@@ -7,24 +6,24 @@ import Control.Monad.Trans
 import Control.Monad.Trans.State.Lazy
 import Prelude hiding ((.), id)
 
---import qualified Graphics.UI.SDL as SDL
---import qualified Graphics.UI.SDL.Primitives as SDL
-
---newtype GameStateTIO a = GameStateTIO (StateT () IO a)
---  deriving (Functor, Monad, MonadIO)
--- wrapGameState = GameStateTIO
+import qualified Graphics.UI.SDL as SDL
+import qualified Graphics.UI.SDL.Events as SDL
 
 type GameStateTIO = StateT () IO
 type MyWire a b = Wire (Timed NominalDiffTime ()) () GameStateTIO a b
 
---startLoop :: MyWire () Int -> GameStateTIO Int
+data GameLoopAction = Quit String
+                    | SdlEvent SDL.Event
+                    | IntResult Int
+                    | TimeResult NominalDiffTime
+
 startLoop wire = do
     (delta, session') <- stepSession clockSession_
     (eitherResult, w') <- stepWire wire delta (Right ())
     liftIO $ putStrLn "In start loop!"
     loop' eitherResult w'
 
-loop' (Left _) _ = return 0
+loop' (Left _) _ = error "Inhibited."
 loop' (Right x) w = do
     liftIO $ putStrLn "In loop!"
     return x
@@ -35,10 +34,20 @@ wire1 = time
 wire2 :: MyWire () Int
 wire2 = for 4 . pure (1 :: Int)
 
-wire3 = mkGen_ $ \x -> do
+wire3 :: MyWire () Int
+wire3 = mkGen_ $ \_ -> do
     liftIO $ putStrLn "wire3"
     return $ Right 10
 
+pollEventWire :: MyWire () SDL.Event
+pollEventWire = mkGen_ $ \_ -> liftM Right (liftIO SDL.pollEvent)
+
+eventMapperWire :: MyWire SDL.Event Int
+eventMapperWire = mkPure_ $ \event -> case event of
+    SDL.NoEvent -> Left ()
+    _ -> Right 100000
+
+solverWire = eventMapperWire . pollEventWire
 
 main :: IO ()
 main = do
@@ -46,8 +55,14 @@ main = do
     res1 <- runStateT (startLoop wire1) ()
     print res1
 
-    res2 <- runStateT (startLoop wire3) ()
+    res2 <- runStateT (startLoop wire2) ()
     print res2
-    
-    testWire clockSession_ (for 1 . wire1)
+
+    res3 <- runStateT (startLoop wire3) ()
+    print res3
+
+    e <- runStateT (startLoop solverWire) ()
+    print e
+
+    --testWire clockSession_ (for 1 . wire1)
     putStrLn "All Ok."
