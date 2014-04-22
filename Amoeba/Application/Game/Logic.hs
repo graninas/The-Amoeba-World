e25c48fb1adfe74e1cef220d9c381a3939733b46
Alexandr Granin <graninas@gmail.com>
2014-04-22 22:17:59 +0700

    - Experiments with wires.

--- Amoeba/Application/Game/Logic.hs
+++ Amoeba/Application/Game/Logic.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Application.Game.Logic where
 
 import Application.Game.Engine.Types
@@ -6,6 +8,7 @@ import Application.Game.Runtime
 import View.View
 
 import Middleware.FRP.NetwireFacade as FRP
+import qualified Control.Wire.Unsafe.Event as E
 import Prelude hiding (id, (.))
 
 import Middleware.Tracing.ErrorHandling
@@ -16,43 +19,63 @@ import Control.Monad.State
 
 type GameWire a b = GWire GameStateTIO a b
 
-data Modes = Mode1 | Mode2 | Mode3 | Finisher
-  deriving (Ord, Eq)
+data Modes = Mode1 | Mode2 | Mode3
+  deriving (Ord, Eq, Show)
+
+data Command = Continue | Finish
+  deriving (Ord, Eq, Show)
 
 logic :: GameWire () ()
-logic = modes Mode1 selector .
-    (
-       diagnose "Some data" &&& (interpret . eSdlEvent --> now . pure Mode3)
-    )
+logic = commandInterpreter . inputInterpreter
 
-selector Mode1 = diagnose "Mode1"
-selector Mode2 = diagnose "Mode2"
-selector Mode3 = diagnose "Mode3"
-selector Finisher = quit
+
+inputInterpreter = for 0.016 . modes Mode1 selector . 
+            (
+               nop &&& interpret . eSdlEvent
+            ) -- --> pure Continue
+
+commandInterpreter = mkGen_ $ \command -> withIO $ print command
+
+nop = pure ()
+
+selector Mode1 = pure Continue
+selector Mode2 = pure Continue
+selector Mode3 = pure Finish
 
 interpret :: GameWire (Event SDL.Event) (Event Modes)
 interpret = mkPure_ $ \event -> Right $ fmap interpreter event
 
+trace :: GameWire (Event Modes) (Event Modes)
+trace = mkGen_ $ \event -> case event of
+    E.NoEvent -> return . Left $ "No netwire event"
+    E.Event e -> do
+        liftIO $ putStrLn $ "Event: " ++ show e
+        return . Right . E.Event $ e
+    
 
-interpreter SDL.Quit = Finisher
-interpreter SDL.MouseButtonUp{} = Mode2
+interpreter SDL.Quit = Mode3
+interpreter SDL.NoEvent = Mode1
+interpreter (SDL.LostFocus _) = Mode2
+interpreter (SDL.GotFocus _) = Mode2
 interpreter SDL.MouseMotion{} = Mode2
 interpreter SDL.MouseButtonDown{} = Mode2
-interpreter _ = Mode1
+interpreter SDL.MouseButtonUp{} = Mode2
+interpreter (SDL.KeyDown _) = Mode2
+interpreter (SDL.KeyUp _) = Mode2
+
 
 eSdlEvent :: GameWire () (Event SDL.Event)
 eSdlEvent = now . pollSdlEvent
-
-
-
 
 diagnose m = mkGen_ $ \_ -> withIO $ putStrLn m
 
 pollSdlEvent :: GameWire () SDL.Event
 pollSdlEvent = mkGen_ $ \_ -> do
     e <- liftIO SDL.pollEvent
-    liftIO $ print e 
-    return $ Right e
+    liftIO $ putStrLn $ "SDL event: " ++ show e
+    return (if e == SDL.NoEvent
+                then Left "SDL: no event."
+                else Right e)
 
 processSdlEvent :: GameWire SDL.Event ()
 processSdlEvent = mkPure_ $ \event -> case event of
