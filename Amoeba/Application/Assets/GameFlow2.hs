c36ec8185a3e6d88ed3dba37d492fd89f7bc36b7
Alexandr Granin <graninas@gmail.com>
2014-04-29 01:25:39 +0700

    - State injection demo #2 is fixed. - GameFlow2 is broken (experiments).

--- Amoeba/Application/Assets/GameFlow2.hs
+++ Amoeba/Application/Assets/GameFlow2.hs
@@ -13,29 +13,36 @@ import Prelude hiding (id, (.))
 
 
 gameNode :: GameNode -> GameWire () ()
-gameNode node = modes Render (selector node) .
+gameNode node = modes (Render, Render) (processor node) .
             (
                 pure () &&& now . interpreter node . pollSdlEvent
             )
 
-selector _    Finish = quit . trace "Finish"
-selector node Render = mkEmpty . render --> gameNode node
-selector _   (SwitchNode swNode) = mkEmpty . render . trace ("Switched to node: " ++ show swNode) --> gameNode swNode
+process _ Finish = quit . trace "Finish"
+process node Render = mkEmpty . render --> gameNode node
+process _ (SwitchNode swNode) = mkEmpty . render . trace ("Switched to node: " ++ show swNode) --> gameNode swNode
 
-interpreter :: GameNode -> GameWire SDL.Event Command
+processor :: GameNode -> (Command, Command) -> GameWire () ()
+processor node (c1, c2) = mkEmpty . process node c1 --> process node c1
+
+interpreter :: GameNode -> GameWire SDL.Event (Command, Command)
 interpreter node = proc event -> do
-    command <- interpretSpecific node -< event
-    returnA -< command
+    command1 <- interpretSpecific node -< event
+    command2 <- interpretCommon -< event
+    returnA -< (command1, command2)
+
+interpretCommon :: GameWire SDL.Event Command
+interpretCommon = mkGen_ $ \e -> case e of
+    SDL.Quit -> retR Finish
+    _ -> retR Render
 
 interpretSpecific :: GameNode -> GameWire SDL.Event Command
 interpretSpecific Screen1 = mkGen_ $ \e -> case e of
-    SDL.Quit -> retR Finish
     (SDL.KeyDown k@(SDL.Keysym SDL.SDLK_SPACE _ _)) -> do
 --        setKeyHolding k
         retR $ SwitchNode Screen2
     _ -> retR Render
 interpretSpecific node = mkGen_ $ \e -> case e of
-    SDL.Quit -> retR Finish
     (SDL.KeyDown (SDL.Keysym SDL.SDLK_SPACE _ _)) -> retR $ SwitchNode (next node)
     _ -> retR $ Render
 
--- Amoeba/Test/StateInjectionTest2.hs
+++ Amoeba/Test/StateInjectionTest2.hs
@@ -4,56 +4,55 @@ import Control.Monad.State
 import Control.Monad
 import System.Random
 
-newtype Context = Context { ctxNextId :: State Context Int
-                          , ctxNextName :: State Context String }
+type Name = String
+data Context = Context { ctxNextId :: State Context Int
+                       , ctxNextName :: State Context Name }
 
 getNextId = get >>= ctxNextId
 getNextName = get >>= ctxNextName
+
+
+-- The state, which will be injected into client code.
+-- You can use nextId to provide sequental int numbers.
+nextId :: Int -> State Context Int
+nextId prevId = do let nId = prevId + 1
+                   modify (\ctx -> ctx { ctxNextId = nextId nId })
+                   return nId
+
+-- Or, instead of nextId, you can use nextRnd to provide random int numbers.
+nextRnd :: StdGen -> State Context Int
+nextRnd prevG = do let (r, g) = randomR (0, 100) prevG
+                   modify (\ctx -> ctx { ctxNextId = nextRnd g })
+                   return r
+
+-- And you can use nextName to provide some names.
+nextName :: Int -> State Context Name
+nextName 0 = do
+    modify (\ctx -> ctx { ctxNextName = nextName 1 } )
+    return "GNVOERK"
+nextName 1 = do
+    modify (\ctx -> ctx { ctxNextName = nextName 2 } )
+    return "RIKTIG YOGLA"
+nextName _ = return "BLABLABLATOR"
 
 
 -- Client code. Knows nothing about furniture materials, but uses external state to create it.
 type IkeaFurniture = (Int, Name)
 
 createFurniture :: State Context IkeaFurniture
-createFurniture = litfM2 (,) getNextId getNextName
+createFurniture = liftM2 (,) getNextId getNextName
 
 ikea :: State Context [IkeaFurniture]
 ikea = do
     table <- createFurniture
     shelf <- createFurniture
     return [table, shelf]
-
-
-
-
-
--- The state, which will be injected into client code.
-nextId :: Int -> State Context Int
-nextId prevId = do let nId = prevId + 1
-                   modify (\ctx -> ctx { ctxNextId = nextId nId })
-                   return nId
-
-nextRnd :: StdGen -> State Context Int
-nextRnd prevG = do let (r, g) = random prevG
-                   modify (\ctx -> ctx { ctxNextId = nextRnd g })
-                   return r
-
-nextName :: Int -> State Context String
-nextName 0 = do
-    modify (\ctx -> ctx { ctxNextName = nextName 1 } )
-    return "ГНВОЕРК"
-nextName 1 = do
-    modify (\ctx -> ctx { ctxNextName = nextName 2 } )
-    return "РИКТИГ ЁГЛА"
-nextName _ = return ""
-
+    
 main :: IO ()
 main = do
-    print "Just increment:"
-    let nextIdF = nextId 0
-    print $ evalState worker (Context nextIdF nextIdF)
+    print "Sequental ids:"
+    print $ evalState ikea (Context (nextId 0) (nextName 0))
     
-    print "Random Id:"
-    let g = mkStdGen 100
-    let nextRndF = nextRnd g
-    print $ evalState worker (Context nextRndF nextRndF)
+    print "Random ids:"
+    print $ evalState ikea (Context (nextRnd (mkStdGen 100)) (nextName 0))
+    
